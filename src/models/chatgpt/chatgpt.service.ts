cafd67cc3f22622ba2d5ccebaf110ca736cd37b5
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-07-09 15:35:50 -0400

    gpt prompt polished

--- src/models/chatgpt/chatgpt.service.ts
+++ src/models/chatgpt/chatgpt.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IPlanRequest, IPlanResponse } from './chatgpt.interfaces';
 import OpenAI from 'openai';
-import { SYSTEM_PROMPT, FUNCTIONS, MAX_TOKENS } from './chatgpt.constants';
+import { generatePrompt, FUNCTIONS, MAX_TOKENS } from './chatgpt.constants';
 
 @Injectable()
 export class ChatGPTService {
@@ -17,12 +17,13 @@ export class ChatGPTService {
   // Get array of tasks from ChatGPT based on goal and weeks
   async getPlanData(request: IPlanRequest): Promise<OpenAI.ChatCompletion> {
     const userPrompt = `Goal: ${request.goal}\nMax weeks: ${request.numWeeks}`;
+    const systemPrompt = generatePrompt(request.numWeeks);
 
     return this.chatgptService.chat.completions.create({
       model: this.configService.get('OPENAI_API_MODEL'),
       response_format: { type: 'json_object' },
       messages: [
-        { role: 'system', content: SYSTEM_PROMPT },
+        { role: 'system', content: systemPrompt },
         { role: 'user', content: userPrompt },
       ],
       functions: FUNCTIONS,
