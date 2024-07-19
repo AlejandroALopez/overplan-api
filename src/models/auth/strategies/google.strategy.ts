4b4030663987937d41613230726cf26729e1871f
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-07-19 16:21:11 -0400

    google strategy URL updated

--- src/models/auth/strategies/google.strategy.ts
+++ src/models/auth/strategies/google.strategy.ts
@@ -13,7 +13,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
-      callbackURL: 'http://localhost:8080/auth/google/callback',
+      callbackURL: configService.get<string>('URL') + '/auth/google/callback',
       scope: ['email', 'profile'],
     });
   }
