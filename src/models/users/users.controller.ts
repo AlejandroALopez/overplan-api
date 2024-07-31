2d3bdd93ec3b669d0aaa677de7d658f09750943a
alejandro <lopez.alejandro@dali.dartmouth.edu>
2024-07-31 11:32:45 -0400

    securing user routes with token marching userId

--- src/models/users/users.controller.ts
+++ src/models/users/users.controller.ts
@@ -2,8 +2,10 @@ import {
   Controller,
   Param,
   Body,
+  Req,
   Get,
   Put,
+  ForbiddenException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User as UserModel } from './schemas/user.schema';
@@ -13,15 +15,31 @@ export class UserController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<UserModel> {
-    return this.usersService.findOneById(id);
+  async findOne(
+    @Param('id') userId: string,
+    @Req() req: any,
+  ): Promise<UserModel> {
+    const tokenUserId = req.user.userId;
+
+    if (userId !== tokenUserId) {
+      throw new ForbiddenException('You are not allowed to get this user.');
+    }
+
+    return this.usersService.findOneById(userId);
   }
 
   @Put(':id')
   async update(
-    @Param('id') id: string,
+    @Param('id') userId: string,
     @Body() user: Partial<UserModel>,
+    @Req() req: any,
   ): Promise<UserModel> {
-    return this.usersService.update(id, user);
+    const tokenUserId = req.user.userId;
+
+    if (userId !== tokenUserId) {
+      throw new ForbiddenException('You are not allowed to update this user.');
+    }
+
+    return this.usersService.update(userId, user);
   }
 }
